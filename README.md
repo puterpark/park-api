60eb5c15795ce01c5053a97af1c889316c8bb9fb
Puter <puterpark@me.com>
2024-02-22 16:39:36 +0900

    feat: 초기 설정

---
+++ README.md
@@ -0,0 +1,7 @@
+# park-api
+업무/개인용 필요한 도구 등을 구현한 공원 API
+
+---
+* `openjdk 21`
+* `spring boot 3.2.2`
+---
---
+++ src/test/java/us/puter/park/ParkApplicationTests.java
@@ -0,0 +1,13 @@
+package us.puter.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
