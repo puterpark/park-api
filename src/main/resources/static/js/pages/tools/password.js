2ba251a47664481f71a7b469ea2b93c1d6186f42
Puter <puterpark@me.com>
2025-02-22 23:49:45 +0900

    PP-19 refactor: 비밀번호 생성기 화면
    - 옵션 버튼을 체크박스로 변경

--- src/main/resources/static/js/pages/tools/password.js
+++ src/main/resources/static/js/pages/tools/password.js
@@ -7,11 +7,11 @@ $(document).ready(() => {
 
 // 이벤트 설정
 const setEvent = () => {
-  toggleBtn('upper', 'secondary')
-  toggleBtn('lower', 'warning')
-  toggleBtn('number', 'success')
-  toggleBtn('special', 'info')
-  toggleBtn('confuse', 'danger')
+  onChangeCheckbox('upper');
+  onChangeCheckbox('lower');
+  onChangeCheckbox('number');
+  onChangeCheckbox('special');
+  onChangeCheckbox('confuse');
 
   const $pwLength = $('#pw-length');
   $pwLength.on('input', () => {
@@ -40,32 +40,35 @@ const copy = () => {
   swal('복사되었습니다.', '', 'success');
 };
 
-// 버튼 토글 이벤트
-const toggleBtn = (id, type) => {
-  const $btn = $('#' + id);
-  $btn.on('click touchstart', (event) => {
-    // 터치 이벤트가 발생했을 때의 기본 동작을 방지
-    if (event.type === 'touchstart') {
-      event.preventDefault();
-    }
+// 체크박스 변경 이벤트
+const onChangeCheckbox = (id) => {
+  const $el = $('#' + id);
+  $el.on('change', function () {
+    const $spans = $(this).siblings('span');
+    const $checkboxes = $('.form-check-input'); // 모든 체크박스 선택
+    const checkedCount = $checkboxes.filter(':checked').length; // 체크된 개수 확인
+    const otherCheckedCount = $checkboxes.not('#confuse').filter(':checked').length; // confuse 제외 체크된 개수
 
-    const anyButtonIsY = $('.btn').filter((_, button) => $(button).val() === 'Y').length <= 1;
-    if ($btn.val() !== 'N' && anyButtonIsY) {
-      return;
-    }
-
-    if ($btn.val() === 'Y') {
-      $btn.removeClass(`btn-${type}`)
-          .addClass(`btn-outline-${type}`)
-          .val('N');
+    if ($(this).is(':checked')) {
+      $spans.css('color', '');
     } else {
-      $btn.removeClass(`btn-outline-${type}`)
-          .addClass(`btn-${type}`)
-          .val('Y');
+      // 체크 해제 전에 최소 하나의 체크박스가 남아 있는지 확인
+      if (checkedCount === 0) {
+        $(this).prop('checked', true); // 다시 체크 상태로 복구
+        return;
+      }
+
+      if ($('#confuse').is(':checked') && otherCheckedCount === 0) {
+        $(this).prop('checked', true); // 다시 체크 상태로 복구
+        return;
+      }
+
+      $spans.css('color', 'var(--primary) !important');
     }
+
     setPassword($('#pw-length').val());
   });
-};
+}
 
 // 비밀번호 생성
 const generatePw = (length) => {
@@ -76,19 +79,19 @@ const generatePw = (length) => {
 
   let chars = '';
 
-  if ($('#upper').val() === 'Y') {
+  if ($('#upper').is(':checked')) {
     chars += upperChar;
   }
-  if ($('#lower').val() === 'Y') {
+  if ($('#lower').is(':checked')) {
     chars += lowerChar;
   }
-  if ($('#number').val() === 'Y') {
+  if ($('#number').is(':checked')) {
     chars += numberChar;
   }
-  if ($('#special').val() === 'Y') {
+  if ($('#special').is(':checked')) {
     chars += specialChar;
   }
-  if ($('#confuse').val() === 'Y') {
+  if ($('#confuse').is(':checked')) {
     const removeChar = "lLIij1!|oO0sS5;:,.`'";
     chars = chars.replace(new RegExp(`[${removeChar}]`, 'g'), '');
   }
